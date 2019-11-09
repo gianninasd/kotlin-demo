50633fc0eb6d9dc5d93d6cce3f981635c6ef29b7
Jimmy G <angelone55@hotmail.com>
2019-11-09 16:11:10 -0500

    cleaned up traversing logic

--- src/main/kotlin/FileParser.kt
+++ src/main/kotlin/FileParser.kt
@@ -1,10 +1,11 @@
-import dg.sample.FileLoader
 import dg.sample.PaymentRecord
 import java.io.File
+import java.time.Duration
+import java.time.LocalDateTime
 import java.util.*
 import java.util.logging.Logger
 
-val logger:Logger = Logger.getLogger("blue")
+val logger:Logger = Logger.getLogger("fileParser")
 
 /**
  * Where application starts
@@ -13,22 +14,35 @@ fun main( args:Array<String> ) {
     val osName = System.getProperty("os.name")
     val osVersion  = System.getProperty("os.version")
 
-    logger.info("Kotlin ${Runtime.version()} File Loader running on $osName $osVersion")
+    logger.info("Kotlin ${KotlinVersion.CURRENT} File Loader running on $osName $osVersion using JVM ${Runtime.version()}")
 
     // load application config
     val config = loadProperties()
+    val workingDir = config.getProperty("client.workingDir")
 
-    val fileLoader = FileLoader(config)
-    fileLoader.readFile()
+    logger.info("Processing files in $workingDir")
 
-    if( args.size == 0 ) {
-        println("Filename missing as argument")
+    File(workingDir).walk().forEach {file ->
+        if(file.isFile) {
+            val fullFileName = file.name
+            var cnt = 0
+            val startTime = LocalDateTime.now()
+
+            logger.info("Processing [$fullFileName] records")
+
+            file.forEachLine {
+                if (it.isNotEmpty()) {
+                    val rec:PaymentRecord = PaymentRecord.parse(it)
+                    println("rec: $rec")
+                    cnt++
+                }
+            }
+
+            val endTime = LocalDateTime.now()
+            val duration = Duration.between(startTime, endTime)
+            logger.info("Finished storing $cnt records in ${duration.seconds} seconds")
+        }
     }
-    else {
-        processFile( args[0] )
-    }
-
-    println("//////////////////////////////")
 }
 
 /**
@@ -37,26 +51,5 @@ fun main( args:Array<String> ) {
 fun loadProperties():Properties {
     val properties = Properties()
     properties.load(ClassLoader.getSystemResourceAsStream("config.properties"))
-    val x = properties.getProperty("client.workingDir")
-    logger.info("x is $x")
     return properties
-}
-
-/**
- * Reads the file provided and parses each record
- */
-fun processFile( fileName:String ) {
-    //logger.info("Parsing file: $fileName")
-    println("Parsing file: $fileName")
-    var totalRecords = 0
-
-    File(fileName).forEachLine {
-        if (it.isNotEmpty()) {
-            val rec:PaymentRecord = PaymentRecord.parse(it)
-            println("rec: $rec")
-            totalRecords++
-        }
-    }
-
-    println("Total records parsed: $totalRecords")
 }
