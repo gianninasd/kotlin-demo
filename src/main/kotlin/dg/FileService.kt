7d3db9334886ee9064c47cc3452e706b551d290e
Jimmy G <angelone55@hotmail.com>
2020-02-28 23:06:54 -0500

    now calculating file hash

--- src/main/kotlin/dg/FileService.kt
+++ src/main/kotlin/dg/FileService.kt
@@ -1,15 +1,14 @@
 package dg
 
+import java.io.File
+import java.security.MessageDigest
+
 /**
  * Service class for all file operations
  */
 class FileService {
 
-  val fileDAO = FileDAO()
-
-  init {
-
-  }
+  private val fileDAO = FileDAO()
 
   /**
    * Extracts only the filename without the extension
@@ -24,13 +23,51 @@ class FileService {
    * Raises DupeFileException if file was previously uploaded in the last 24 hrs
    */
   fun create( workingDir:String, fileName:String ):Int {
-    //val fileHash = calculateHash(workingDir, fileName)
+    val fileHash = calculateHash(workingDir, fileName)
     //val cnt = fileDAO.countInLast24hrs(fileHash)
-    val fileId = fileDAO.create(fileName, "xx"/*fileHash*/)
+    val fileId = fileDAO.create(fileName, fileHash)
 
     /*if( cnt > 0 )
       throw new DupeFileException()*/
 
     return fileId
+  }
+
+  /**
+   * Generates the hash value of the file contents
+   */
+  private fun calculateHash( workingDir:String, fileName:String ):String {
+    val file = File("$workingDir/$fileName")
+    val digest = MessageDigest.getInstance("SHA-1")
+    val inputStream = file.inputStream()
+    val buffer = ByteArray(16384)
+    var len:Int
+
+    while( true ) {
+      len = inputStream.read(buffer)
+
+      if( len <= 0 ) {
+        break
+      }
+
+      digest.update(buffer, 0, len)
+    }
+
+    val sha1sum = digest.digest()
+    var result = ""
+
+    for( b in sha1sum) {
+      result += toHex(b.toInt())
+    }
+
+    return result
+  }
+
+  private fun hexChr(b:Int):String {
+    return Integer.toHexString(b and 0xF)
+  }
+
+  private fun toHex(b:Int):String {
+    return hexChr((b and 0xF0) shr 4) + hexChr(b and 0x0F)
   }
 }
